1d12b5200333d717b52df9e9f47c176be02d321a
akhil-pws <akhil.a@pacewisdom.com>
2023-08-08 13:48:25 +0530

    stage 1

--- src/app/output/output.component.ts
+++ src/app/output/output.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { counterState } from '../state/counter.state';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-output',
@@ -8,13 +9,16 @@ import { counterState } from '../state/counter.state';
   styleUrls: ['./output.component.scss']
 })
 export class OutputComponent implements OnInit {
-counter:number =0;
-constructor(private store:Store<{count :counterState}>){
+// counter:number =0;
+counter$!: Observable<{ counter: number }>;
+constructor(private store:Store<{count:counterState}>){
 
 }
   ngOnInit(): void {
-   this.store.select('count').subscribe((data)=>{
-    this.counter =data.counter
-   })
+  //  this.store.select('counter').subscribe((data)=>{
+  //   this.counter =data.counter
+  //  })
+
+  this.counter$=this.store.select('count')
   }
 }
